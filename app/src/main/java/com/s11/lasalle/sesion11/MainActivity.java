60ffd453546a2e59e68de789472984ddf22b545e
alexbruch <alexbruch.cat@gmail.com>
2017-02-03 11:21:10 +0100

    Proves

--- app/src/main/java/com/s11/lasalle/sesion11/MainActivity.java
+++ app/src/main/java/com/s11/lasalle/sesion11/MainActivity.java
@@ -1,15 +1,22 @@
 package com.s11.lasalle.sesion11;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
@@ -17,6 +24,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -34,6 +42,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},21
+                );
+            }
+        }
 
         link = (EditText) findViewById(R.id.urlIMG);
         download = (Button) findViewById(R.id.downloadimg);
@@ -111,12 +133,11 @@ public class MainActivity extends AppCompatActivity {
         bigPictureStyle.bigPicture(bitmap);
         builder.setStyle(bigPictureStyle);
 
-        /** COMPARTIR IMATGE  **/
-        File file = new File(Environment.DIRECTORY_PICTURES, imageName);
+        /** COMPARTIR IMATGE EN CACHÉ **/
+        File file = new File(getApplicationContext().getCacheDir(), imageName);
         try {
-
             FileOutputStream fileOutputStream = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
             fileOutputStream.flush(); // Optimizació: per borrar fluxe de bytes de sortida i obligar als que estan en memoria a escriures
             fileOutputStream.close();
             file.setReadable(true, false); // Per permetre acces de lectura a totes les aplicacions
@@ -128,12 +149,15 @@ public class MainActivity extends AppCompatActivity {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
-        shareIntent.setType("image/*");
-        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 1, shareIntent, 0);
-        builder.addAction(android.R.drawable.ic_menu_share, "SHARE", pendingIntent);
-        builder.setContentIntent(pendingIntent);
+        shareIntent.setType("image/jpeg");
+        startActivity(shareIntent);
+        //PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 1, shareIntent, 0);
+        //builder.addAction(android.R.drawable.ic_menu_share, "SHARE", pendingIntent);
+        //builder.setContentIntent(pendingIntent);
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, builder.build());
+
     }
+
 }
 
